f75eb8faa4d0d6c8adc6107d03a0b89324d09961
Ilya2502 <gavran2502@mail.ru>
2022-01-09 11:48:45 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let nStr = n.toString();
+    let result = '';
+    for (let i = 0; i < nStr.length; i++) {
+        result = `${result}${nStr[nStr.length - 1 - i]}`;
+    }
+    return Number(result);
 }
